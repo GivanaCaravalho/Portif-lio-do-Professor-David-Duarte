cc51ab62131fe142a1552fe821be130ac242eb3e
Gio <149380831+GivanaCaravalho@users.noreply.github.com>
2023-12-17 12:40:21 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+
+       
+function changeTab(index) {
+    var tabs = document.querySelectorAll('.tab');
+
+    tabs.forEach(function(tab) {
+        tab.classList.remove('active');
+        });
+
+        tabs[index].classList.add('active');
+        }
+
+changeTab(0);
+    
